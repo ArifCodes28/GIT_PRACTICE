c55eb0a9dd12940d4f627f5b9e8e4c6b01921d3e
Md Arif <mearif83@gmail.com>
2026-04-04 15:38:02 +0000

    corrected code

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 
-<<<<<<< HEAD
 print("Main updated")
-=======
 print("Rebase feature")
->>>>>>> c7cbc69 (Rebase feature commit)
+#>>>>>>> c7cbc69 (Rebase feature commit)
 for i in range(1,10):
 	print(i)
